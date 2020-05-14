0257a072ec185bd877be17653d8d8f2e96675a04
Mau <mauricio.sdjr@gmail.com>
2020-05-14 11:49:54 -0300

    fix exercises names

---
+++ exercises/4-javascript/block-1/05-exercise.js
@@ -0,0 +1,7 @@
+let angleOne = 60, angleTwo = 70, angleThree = 60;
+
+if(angleOne + angleTwo + angleThree === 180) {
+    console.log(true);
+} else {
+    console.log("ERROR: this is not a valid triangle");
+}
