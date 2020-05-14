f3ebc43b8224fc0f86b82eee2d36fa37eeccc6c5
Mau <mauricio.sdjr@gmail.com>
2020-05-14 11:48:00 -0300

    fix exercises names

--- exercises/4-javascript/block-1/2-exercise.js
+++
@@ -1,9 +0,0 @@
-let a = 3, b = 5;
-
-if(a > b) {
-    console.log(a);
-} else if(b > a) {
-    console.log(b);
-} else {
-    console.log("The numbers are equal")
-}
--- exercises/4-javascript/block-1/5-exercise.js
+++
@@ -1,7 +0,0 @@
-let angleOne = 60, angleTwo = 70, angleThree = 60;
-
-if(angleOne + angleTwo + angleThree === 180) {
-    console.log(true);
-} else {
-    console.log("ERROR: this is not a valid triangle");
-}
