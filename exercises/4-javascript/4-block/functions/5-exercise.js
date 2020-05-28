06566512e85ded21104992c0afca44670496c2a6
Mau <mauricio.sdjr@gmail.com>
2020-05-28 16:13:06 -0300

    new exercises

---
+++ exercises/4-javascript/4-block/functions/5-exercise.js
@@ -0,0 +1,10 @@
+/*
+Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+*/
+
+let numbers = [2, 3, 2, 5, 8, 2, 3];
+
+function mostRepeatedNumber() {
+    
+
+console.log(mostRepeatedNumber(numbers));
